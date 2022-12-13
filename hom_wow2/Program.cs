b78b003a60639b6a055b8754c9528a6babd981bd
Doston <119062575+dosrih@users.noreply.github.com>
2022-12-13 17:57:35 +0500

    Add files via upload

---
+++ hom_wow2/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberA = new Random().Next(1, 100); // 1 2 3 4 ... 100
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 100);
+Console.WriteLine(numberC);
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+
+if (numberC > max)
+{
+    max = numberC;
+}
+
+Console.WriteLine(max);
