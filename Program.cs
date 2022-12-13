2e488ab9e08785c50b094f3da62ba7de9a67188c
doston <rikhsiboevdoston@email.com>
2022-12-13 15:57:38 +0500

    hello

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Int32.Parse takes a string, and returns an int, not a string:
+int n1 = Int32.Parse("5");
+int n2 = Int32.Parse("8");
+// Math.Min and Math.Max functions pick the min and max
+int min = Math.Min(n1, n2);
+int max = Math.Max(n1, n2);
+// Show both numbers in a message box in one go using String.Format:
+Console.WriteLine(string.Format("Min:{0} Max:{1}", min, max));
