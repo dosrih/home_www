519e05028eafadc45085741308975fbd19fa5e5e
Doston <119062575+dosrih@users.noreply.github.com>
2022-12-13 17:58:53 +0500

    Add files via upload

---
+++ hom _wow3/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(1, 100); // 1 2 3 4 ... 100
+Console.WriteLine(number);
+
+if (number % 2 == 1)
+{
+    Console.WriteLine("ODD");
+}
+else
+{
+    Console.WriteLine("EVEN");
+};
